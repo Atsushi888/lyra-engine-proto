f3916bc65bb413a4a8b147b5a8deb0d3b6ca1618
Atsushi Nonaka <92051396+Atsushi888@users.noreply.github.com>
2025-11-13 03:09:48 +0900

    Disable UserWindowPanel rendering in PrivateView
    Comment out the UserWindowPanel rendering in PrivateView.

--- views/private_view.py
+++ views/private_view.py
@@ -1,9 +1,9 @@
 # views/private_view.py
 from __future__ import annotations
 import streamlit as st
-from components.user_window import UserWindowPanel
+# from components.user_window import UserWindowPanel
 
 class PrivateView:
     def render(self) -> None:
         st.info("※ 公開版では表示されません。開発者専用。")
-        UserWindowPanel().render()
+        # UserWindowPanel().render()
